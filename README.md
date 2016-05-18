2997a6c42fdb72d1f1518b9f7f7227576bd83a2b
Nicholas Major <nick@nmajor.com>
2016-05-19 03:12:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### Development
+
+First install node modules
+
+    npm install
+
+Then start the dev server
+
+    npm start
+
+
+Only edit files src/* or index.html
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config.js');
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+
+app.use('/assets/images', express.static(__dirname + '/assets/images'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
