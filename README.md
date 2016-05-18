d8dfb06af2f5204007de25bc70b5d9ed53d442c0
Nicholas Major <nick@nmajor.com>
2016-05-19 06:31:29 +0700

    Forgot to build the source files.

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Then start the dev server
 
 
 Only edit files src/* or index.html
+
+
+IMPORTANT Be sure to build before pushing to master
+
+    npm run build
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config.js');
+var config = require('./webpack.config.dev.js');
 var compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
